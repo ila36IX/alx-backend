fee5e6c940677f99131e50a78b9b091e884ce208
ila36IX <jbariali002@gmail.com>
2024-09-22 22:28:41 +0100

    Force locale with URL parameter

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Adding the `getlocale` function to determine the best match language
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, refresh
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+class Config:
+    """The config class for flask application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', strict_slashes=False)
+def hello_i18n():
+    """single page using flask"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x02-i18n/app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Adding the `getlocale` function to determine the best match language
+"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+@babel.localeselector
+def get_locale():
+    """The config class for flask application"""
+    request_lang = request.args.get('locale', None)
+    if request_lang and request_lang in app.config['LANGUAGES']:
+        return request_lang
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+class Config:
+    """The config class for flask application"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+def get_user():
+    """Returns a user dictionary or None if the ID cannot be found or if
+    login_as was not passed
+    """
+    user_id = request.args.get('login_as', None) 
+    if user_id is None or not user_id.isdigit():
+        return None
+    return users.get(int(user_id))
+
+
+@app.before_request
+def middleware():
+    """find loged in user, and set it as a global on flask.g.user"""
+    g.user = get_user()
+
+
+@app.route('/', strict_slashes=False)
+def hello_i18n():
+    """single page using flask"""
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    """Main Function"""
+    app.run(host='0.0.0.0', port=5000)
